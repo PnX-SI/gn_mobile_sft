c21cf6ec65d5eda472c1d8f63c7210ed7a980f13
robin ELZEARD <robinelzeard@gmail.com>
2019-03-06 16:40:53 +0100

    installation de leaflet et début de bidouillage

--- src/app/start-input/start-input.page.ts
+++ src/app/start-input/start-input.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-start-input',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-input.page.scss'],
 })
 export class StartInputPage implements OnInit {
+	
+	map:L.Map;
+	
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() 
+	{
+		this.map = new L.Map('map', {
+		center:[46.52863469527167, 2.43896484375],
+		zoom: 6
+		});
+		
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		   // tslint:disable-next-line
+		  attribution: '&copy; OpenStreetMap',
+		  maxZoom: 18
+		}).addTo(this.map);
+	}
 
 }
