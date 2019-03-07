810a56ed08b00e0ffdbe8e37031b370033a33239
robin ELZEARD <robinelzeard@gmail.com>
2019-03-07 10:08:30 +0100

    bouton reload + gestion pour si la géolocalisation est active ou non

--- src/app/start-input/start-input.page.ts
+++ src/app/start-input/start-input.page.ts
@@ -10,20 +10,44 @@ export class StartInputPage implements OnInit {
 	
 	map:L.Map;
 	
-	constructor() { }
+	constructor() {
+	}
 
 	ngOnInit() 
 	{
-		this.map = new L.Map('map', {
-		center:[46.52863469527167, 2.43896484375],
-		zoom: 6
+		this.map = new L.Map('map');
+		this.map.locate({
+		setView: true, 
+		maxZoom: 11
 		});
+		this.map.on('locationfound', (e)=> {this.onLocationFound(e)});
+		this.map.on('locationerror', (e)=> {this.onLocationError(e)});
 		
+		
+				
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		   // tslint:disable-next-line
 		  attribution: '&copy; OpenStreetMap',
 		  maxZoom: 18
 		}).addTo(this.map);
+		
+		
 	}
-
+	
+	reload()
+	{
+		this.map.invalidateSize();
+	}
+	
+	onLocationFound(e) {
+		console.log("localisation trouvée");
+	}
+	
+	onLocationError(e) {
+    	alert(e.message + "\rWe'll show default map");
+    	this.map.setView(
+		/*centre*/[46.52863469527167, 2.43896484375],
+		/*zoom*/6
+		);
+	}
 }
