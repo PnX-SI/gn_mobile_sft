ef0fb5c863b6efe5589406bd8ee6b659b5d053a1
robin ELZEARD <robinelzeard@gmail.com>
2019-03-11 11:01:20 +0100

    ajout d'un gestionnaire de préférences

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import {Router} from '@angular/router';
 
 import { OfflineManagerService } from './services/offline-manager.service';
+import { AppPreferences } from '@ionic-native/app-preferences/ngx';
+
 
 
 @Component({
@@ -22,7 +24,8 @@ export class AppComponent {
 	private statusBar: StatusBar,
   private router: Router,
   private offlineManager: OfflineManagerService,
-  private networkService: NetworkService
+  private networkService: NetworkService,
+  private appPreferences: AppPreferences
   ) {
 	this.initializeApp();
   }
@@ -35,6 +38,11 @@ export class AppComponent {
       if (status == ConnectionStatus.Online) {
         this.offlineManager.checkForEvents().subscribe();
       }
+    this.appPreferences.fetch('key').then((res) =>
+    {
+      alert(res);
+    })
+
     });
 	});
   }
