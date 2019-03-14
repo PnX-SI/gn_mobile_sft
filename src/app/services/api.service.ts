43d486096282b030606897564d0ad1d03c8a3b5e
robin ELZEARD <robinelzeard@gmail.com>
2019-03-14 08:48:13 +0100

    récupération des données via la vrai API

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,7 +8,7 @@ import { tap, map, catchError } from "rxjs/operators";
  
 const API_STORAGE_KEY = 'specialkey';
 //const API_URL = 'http://demo.geonature.fr/geonature/api/sft';
-const API_URL = 'http://demo.geonature.fr/geonature/api/sft';
+const API_URL = 'http://51.75.122.69/geonature/api/suivi_flore_territoire';
  
 @Injectable({
   providedIn: 'root'
@@ -30,7 +30,7 @@ export class ApiService {
     } else {      
       // Return real API data and store it locally
       console.log("load data")
-      return this.http.get(`${API_URL}/sites?id_application=6&id_area_type=25`).pipe(
+      return this.http.get(`${API_URL}/sites?id_application=7&id_area_type=25`).pipe(
         map(res => 
           res['features']
         ),
