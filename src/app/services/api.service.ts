83e91448e02303a1311589baa46687519d54b2b2
robin ELZEARD <robinelzeard@gmail.com>
2019-04-23 14:54:49 +0200

    tentative de régler les problèmes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,11 +1,11 @@
 import { OfflineManagerService } from './offline-manager.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http'
 import { NetworkService, ConnectionStatus } from './network.service';
 import { Storage } from '@ionic/storage';
 import { Observable, from } from 'rxjs';
 import { tap, map, catchError } from "rxjs/operators";
- 
+  
 //const API_STORAGE_KEY = 'specialkey';
 const API_URL = 'http://demo.geonature.fr/geonature/api'; //API test
 //const API_URL = 'http://51.75.122.69/geonature/api'; //API prod
