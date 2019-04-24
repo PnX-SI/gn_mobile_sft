5fc5a5384d4f0022aad5a9256f152a93dad113e6
robin ELZEARD <robinelzeard@gmail.com>
2019-04-24 09:52:15 +0200

    l'api est maintenant lue dans settings.json

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,27 +5,32 @@ import { NetworkService, ConnectionStatus } from './network.service';
 import { Storage } from '@ionic/storage';
 import { Observable, from } from 'rxjs';
 import { tap, map, catchError } from "rxjs/operators";
+import {LocalVariablesService} from './local-variables.service'
   
-//const API_STORAGE_KEY = 'specialkey';
-const API_URL = 'http://demo.geonature.fr/geonature/api'; //API test
+//const API_URL = 'http://demo.geonature.fr/geonature/api'; //API test
 //const API_URL = 'http://51.75.122.69/geonature/api'; //API prod
-const API_REPO = 'sft' //API test
+//const API_REPO = 'sft' //API test
 //const API_REPO = 'suivi_flore_territoire' //API prod
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   
+  //API_URL = this.local.getSettings()['API_URL']
+  //API_REPO = this.local.getSettings()['API_Dir']
+
   constructor
   (
     private http: HttpClient, 
     private networkService: NetworkService, 
     private storage: Storage, 
     private offlineManager: OfflineManagerService,
+    private local: LocalVariablesService
     
-    ) { }
+    ) {
+
+    }
  
   getData(forceRefresh: boolean = false, requeteType: string = "base", id: number = 0): Observable<any> {
     if (this.networkService.getCurrentNetworkStatus() == ConnectionStatus.Offline || !forceRefresh) {
@@ -56,7 +61,8 @@ export class ApiService {
       
       if (requeteType == "base")
       {
-        return this.http.get(`${API_URL}/${API_REPO}/sites?id_application=7&id_area_type=25`).pipe(
+        console.log("api url:"+this.local.getSettings()['API_URL']+" api dir:"+this.local.getSettings()['API_Dir'])
+        return this.http.get(`${this.local.getSettings()['API_URL']}/${this.local.getSettings()['API_Dir']}/sites?id_application=7&id_area_type=25`).pipe(
           map(res => 
             res['features']
           ),
@@ -69,7 +75,7 @@ export class ApiService {
       }
       else if (requeteType == "visite")
       {
-        return this.http.get(`${API_URL}/${API_REPO}/sites?id_base_site=${id}`).pipe(
+        return this.http.get(`${this.local.getSettings()['API_URL']}/${this.local.getSettings()['API_Dir']}/sites?id_base_site=${id}`).pipe(
           map(res => 
             res['features']
           ),
@@ -82,7 +88,7 @@ export class ApiService {
       }
       else if (requeteType == "maille")
       {
-        return this.http.get(`${API_URL}/gn_monitoring/siteareas/${id}?id_area_type=32`).pipe(
+        return this.http.get(`${this.local.getSettings()['API_URL']}/gn_monitoring/siteareas/${id}?id_area_type=32`).pipe(
           map(res => 
             res['features']
           ),
@@ -95,7 +101,7 @@ export class ApiService {
       }
       else if (requeteType == "observeur")
       {
-        return this.http.get(`${API_URL}/users/menu/1`).pipe(
+        return this.http.get(`${this.local.getSettings()['API_URL']}/users/menu/1`).pipe(
           map(res =>
             [res]
           ),
@@ -108,7 +114,7 @@ export class ApiService {
       }
       else if (requeteType == "perturbations")
       {
-        return this.http.get(`${API_URL}/nomenclatures/nomenclature/TYPE_PERTURBATION?regne=&group2_inpn=&orderby=label_default`).pipe(
+        return this.http.get(`${this.local.getSettings()['API_URL']}/nomenclatures/nomenclature/TYPE_PERTURBATION?regne=&group2_inpn=&orderby=label_default`).pipe(
           map(res =>
             res['values']
           ),
@@ -146,7 +152,7 @@ export class ApiService {
       id_application: 3
     }
     //TODO: quand on pourra récupérer le token, le stocker au lieu de user
-    this.http.post(`${API_URL}/auth/login`,user,{headers:{}, observe: "response" as "response", withCredentials : true})
+    this.http.post(`${this.local.getSettings()['API_URL']}/auth/login`,user,{headers:{}, observe: "response" as "response", withCredentials : true})
     .pipe(
       catchError(err =>{ throw err})
     )
