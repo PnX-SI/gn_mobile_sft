4bf0ef4e43cd9aa6e5ef4863ab1c83b2bf148a94
robin ELZEARD <robinelzeard@gmail.com>
2019-06-07 15:25:09 +0200

    gestion envoi de données (base)

--- src/app/see-visits/see-visits.page.ts
+++ src/app/see-visits/see-visits.page.ts
@@ -1,64 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Storage } from '@ionic/storage';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Storage } from "@ionic/storage";
+import { ApiService } from "../services/api.service";
 
 @Component({
-  selector: 'app-see-visits',
-  templateUrl: './see-visits.page.html',
-  styleUrls: ['./see-visits.page.scss'],
+  selector: "app-see-visits",
+  templateUrl: "./see-visits.page.html",
+  styleUrls: ["./see-visits.page.scss"]
 })
 export class SeeVisitsPage implements OnInit {
+  visites = [];
 
-  visites = []
-
-  constructor
-  (
+  constructor(
     private router: Router,
-    private storage: Storage
-  ) 
-  { 
+    private storage: Storage,
+    private apiService: ApiService
+  ) {}
 
-  }
+  ngOnInit() {}
 
-  ngOnInit() 
+  ionViewDidEnter() //quand on rentre dans la page
   {
-
+    //récupération du nombre de visites non synchronisées
+    this.visites = []; //reset pour éviter un faux positif
+    for (var i = 0; i <= 99; i++) {
+      this.storage.get("visiteSite" + i).then(res => {
+        if (res) {
+          this.visites.push(res);
+        }
+      });
+    }
+    console.log(this.visites);
   }
 
-  ionViewDidEnter()//quand on rentre dans la page
-	{
-		//récupération du nombre de visites non synchronisées
-		this.visites = [] //reset pour éviter un faux positif
-		for (var i = 0;i <=99;i++)
-		{
-			this.storage.get("visiteSite"+i).then ((res)=>{
-				if(res)
-				{
-					this.visites.push(res)
-				}
-			})
-		}
-		console.log(this.visites)
-  }
-  
-  GoToHome()
-	{
-		//on envoi l'utilisateur sur la page d'acceuil
-		this.router.navigate(['/home']);
-  }
-  
-  watch(id)
-  {
-    this.router.navigate(['/see-my-visit',{id:id}]);
+  GoToHome() {
+    //on envoi l'utilisateur sur la page d'acceuil
+    this.router.navigate(["/home"]);
   }
 
-  delete(id)
-  {
-    var confirmation = confirm("êtes vous sûr de vouloir supprimer cette visite?")
-    if(confirmation)
-    {
-      this.storage.remove("visiteSite"+id)
-      this.ionViewDidEnter()
+  watch(id) {
+    this.router.navigate(["/see-my-visit", { id: id }]);
+  }
+
+  delete(id) {
+    var confirmation = confirm(
+      "êtes vous sûr de vouloir supprimer cette visite?"
+    );
+    if (confirmation) {
+      this.storage.remove("visiteSite" + id);
+      this.ionViewDidEnter();
+    }
+  }
+
+  send(id) {
+    var confirmation = confirm(
+      "êtes vous sûr de vouloir envoyer cette visite?"
+    );
+    if (confirmation) {
+      this.storage.get("visiteSite" + id).then(data => {
+        this.storage.get("user").then(user => {
+          if (user["access_token"]) {
+            this.apiService.sendData(user["access_token"], data);
+            this.ionViewDidEnter();
+          } else {
+            console.error("pas de token");
+          }
+        });
+      });
     }
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,204 +1,226 @@
-import { OfflineManagerService } from './offline-manager.service';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { NetworkService, ConnectionStatus } from './network.service';
-import { Storage } from '@ionic/storage';
-import { Observable, from } from 'rxjs';
+import { OfflineManagerService } from "./offline-manager.service";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { NetworkService, ConnectionStatus } from "./network.service";
+import { Storage } from "@ionic/storage";
+import { Observable, from } from "rxjs";
 import { tap, map, catchError } from "rxjs/operators";
-import {LocalVariablesService} from './local-variables.service'
-  
+import { LocalVariablesService } from "./local-variables.service";
+
 //const API_URL = 'http://demo.geonature.fr/geonature/api'; //API test
 //const API_URL = 'http://51.75.122.69/geonature/api'; //API prod
 //const API_REPO = 'sft' //API test
 //const API_REPO = 'suivi_flore_territoire' //API prod
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ApiService {
-  
-  constructor
-  (
-    private http: HttpClient, 
-    private networkService: NetworkService, 
-    private storage: Storage, 
+  constructor(
+    private http: HttpClient,
+    private networkService: NetworkService,
+    private storage: Storage,
     private offlineManager: OfflineManagerService,
     private local: LocalVariablesService
-    
+  ) {}
+
+  getData(
+    forceRefresh: boolean = false,
+    requeteType: string = "base",
+    id: number = 0
+  ): Observable<any> {
+    if (
+      this.networkService.getCurrentNetworkStatus() ==
+        ConnectionStatus.Offline ||
+      !forceRefresh
     ) {
-    
-    }
- 
-  getData(forceRefresh: boolean = false, requeteType: string = "base", id: number = 0): Observable<any> {
-    if (this.networkService.getCurrentNetworkStatus() == ConnectionStatus.Offline || !forceRefresh) {
       // Return the cached data from Storage
-      if (requeteType == "base")
-      {
-        return from(this.getLocalData('base'));
+      if (requeteType == "base") {
+        return from(this.getLocalData("base"));
+      } else if (requeteType == "maille") {
+        return from(this.getLocalData("maille" + id));
+      } else if (requeteType == "visite") {
+        return from(this.getLocalData("visite" + id));
+      } else if (requeteType == "observeur") {
+        return from(this.getLocalData("observeur"));
+      } else if (requeteType == "perturbations") {
+        return from(this.getLocalData("perturbations"));
+      } else if (requeteType == "organisme") {
+        return from(this.getLocalData("organisme"));
       }
-      else if (requeteType == "maille")
-      {
-        return from(this.getLocalData('maille'+id));
-      }
-      else if (requeteType == "visite")
-      {
-        return from(this.getLocalData('visite'+id));
-      }
-      else if(requeteType == "observeur")
-      {
-        return from(this.getLocalData('observeur'))
-      }
-      else if(requeteType == "perturbations")
-      {
-        return from(this.getLocalData('perturbations'))
-      }
-      else if(requeteType == "organisme")
-      {
-        return from(this.getLocalData('organisme'))
-      }
-
-    } else {      
-      // Return real API data
-      
-      if (requeteType == "base")
-      {
-        //console.log("api url:"+this.local.getSettings()['API_URL']+" api dir:"+this.local.getSettings()['API_Dir'])
-        return this.http.get(`${this.local.getSettings()['API_URL']}/${this.local.getSettings()['API_Dir']}/sites?id_application=7&id_area_type=25`).pipe(
-          map(res => 
-            res['features']
-          ),
-          catchError(err => {
-            console.error(err);
-            console.log("renvoi des données locales");
-
-            return from(this.getLocalData('base'));
-          })
-        )
-      }
-      else if (requeteType == "visite")
-      {
-        return this.http.get(`${this.local.getSettings()['API_URL']}/${this.local.getSettings()['API_Dir']}/sites?id_base_site=${id}`).pipe(
-          map(res => 
-            res['features']
-          ),
-          catchError(err => {
-            console.error(err);
-            console.log("renvoi des données locales");
-            return from(this.getLocalData('visite'+id));
-          })
-        )
-      }
-      else if (requeteType == "maille")
-      {
-        return this.http.get(`${this.local.getSettings()['API_URL']}/gn_monitoring/siteareas/${id}?id_area_type=32`).pipe(
-          map(res => 
-            res['features']
-          ),
-          catchError(err => {
-            console.error(err);
-            console.log("renvoi des données locales");
-            return from(this.getLocalData('maille'+id));
-          })
-        )
-      }
-      else if (requeteType == "observeur")
-      {
-        return this.http.get(`${this.local.getSettings()['API_URL']}/users/menu/1`).pipe(
-          map(res =>
-            [res]
-          ),
-          catchError(err => {
-            console.error(err);
-            console.log("renvoi des données locales");
-            return from(this.getLocalData('observeur'));
-          })
-        )
-      }
-      else if (requeteType == "perturbations")
-      {
-        return this.http.get(`${this.local.getSettings()['API_URL']}/nomenclatures/nomenclature/TYPE_PERTURBATION?regne=&group2_inpn=&orderby=label_default`).pipe(
-          map(res =>
-            res['values']
-          ),
-          catchError(err => {
-            console.error(err);
-            console.log("renvoi des données locales");
-            return from(this.getLocalData('perturbations'));
-          })
-        )
-      }
-      else if (requeteType == "organisme")
-      {
-        return this.http.get(`${this.local.getSettings()['API_URL']}/${this.local.getSettings()['API_Dir']}/organisme`).pipe(
-          map(res =>
-            res
-          ),
-          catchError(err => {
-            console.error(err);
-            console.log("renvoi des données locales");
-            return from(this.getLocalData('organisme'));
-          })
-        )
-      }
-    }
-  }
- 
-  /*updateUser(user, data): Observable<any> {
-    let url = `${API_URL}/users/${user}`;
-    if (this.networkService.getCurrentNetworkStatus() == ConnectionStatus.Offline) {
-      return from(this.offlineManager.storeRequest(url, 'PUT', data));
     } else {
-      return this.http.put(url, data).pipe(
-        catchError(err => {
-          this.offlineManager.storeRequest(url, 'PUT', data);
-          throw new Error(err);
-        })
-      );
-    }
-  }*/
+      // Return real API data
 
-  public LogInAPI(log, pswd)
-  {
-    reponse = null
-    const user = {
-      login:log,
-      password:pswd,
-      id_application: 3
-    }
-    this.http.post(`${this.local.getSettings()['API_URL']}/auth/login`,user,{headers:{}, observe: "response" as "response", withCredentials : true})
-    .pipe(
-      catchError(err =>{ throw err})
-    )
-    .subscribe(
-      data=>{
-        this.storage.set("user",data.body)
-        reponse = data
-        if(data.body["access_token"])
-        {
+      if (requeteType == "base") {
+        //console.log("api url:"+this.local.getSettings()['API_URL']+" api dir:"+this.local.getSettings()['API_Dir'])
+        return this.http
+          .get(
+            `${this.local.getSettings()["API_URL"]}/${
+              this.local.getSettings()["API_Dir"]
+            }/sites?id_application=7&id_area_type=25`
+          )
+          .pipe(
+            map(res => res["features"]),
+            catchError(err => {
+              console.error(err);
+              console.log("renvoi des données locales");
 
-        }
-        else
-        {
-          console.warn("nous n'arrivons pas a trouver le token.\nModifiez l'api de manière à ce qu'elle renvoi le token")
-        }
-      },
-      err =>{
-        reponse = err
+              return from(this.getLocalData("base"));
+            })
+          );
+      } else if (requeteType == "visite") {
+        return this.http
+          .get(
+            `${this.local.getSettings()["API_URL"]}/${
+              this.local.getSettings()["API_Dir"]
+            }/sites?id_base_site=${id}`
+          )
+          .pipe(
+            map(res => res["features"]),
+            catchError(err => {
+              console.error(err);
+              console.log("renvoi des données locales");
+              return from(this.getLocalData("visite" + id));
+            })
+          );
+      } else if (requeteType == "maille") {
+        return this.http
+          .get(
+            `${
+              this.local.getSettings()["API_URL"]
+            }/gn_monitoring/siteareas/${id}?id_area_type=32`
+          )
+          .pipe(
+            map(res => res["features"]),
+            catchError(err => {
+              console.error(err);
+              console.log("renvoi des données locales");
+              return from(this.getLocalData("maille" + id));
+            })
+          );
+      } else if (requeteType == "observeur") {
+        return this.http
+          .get(`${this.local.getSettings()["API_URL"]}/users/menu/1`)
+          .pipe(
+            map(res => [res]),
+            catchError(err => {
+              console.error(err);
+              console.log("renvoi des données locales");
+              return from(this.getLocalData("observeur"));
+            })
+          );
+      } else if (requeteType == "perturbations") {
+        return this.http
+          .get(
+            `${
+              this.local.getSettings()["API_URL"]
+            }/nomenclatures/nomenclature/TYPE_PERTURBATION?regne=&group2_inpn=&orderby=label_default`
+          )
+          .pipe(
+            map(res => res["values"]),
+            catchError(err => {
+              console.error(err);
+              console.log("renvoi des données locales");
+              return from(this.getLocalData("perturbations"));
+            })
+          );
+      } else if (requeteType == "organisme") {
+        return this.http
+          .get(
+            `${this.local.getSettings()["API_URL"]}/${
+              this.local.getSettings()["API_Dir"]
+            }/organisme`
+          )
+          .pipe(
+            map(res => res),
+            catchError(err => {
+              console.error(err);
+              console.log("renvoi des données locales");
+              return from(this.getLocalData("organisme"));
+            })
+          );
       }
-    )
+    }
   }
- 
+
+  sendData(token, data) {
+    let url = `${this.local.getSettings()["API_URL"]}/${
+      this.local.getSettings()["API_Dir"]
+    }/visitJWT`;
+    if (
+      this.networkService.getCurrentNetworkStatus() == ConnectionStatus.Offline
+    ) {
+      alert(
+        "Vous n'êtes pas connecté a internet. Les données n'ont pas été envoyées"
+      );
+      return null;
+    } else {
+      return this.http
+        .post(url, data, {
+          headers: { jwt: token }
+        })
+        .pipe(
+          catchError(err => {
+            throw err;
+          })
+        )
+        .subscribe(
+          mes => {
+            console.log("données envoyées");
+            console.log(mes);
+          },
+          err => {
+            console.error(err);
+          }
+        );
+    }
+  }
+
+  public LogInAPI(log, pswd) {
+    reponse = null;
+    const user = {
+      login: log,
+      password: pswd,
+      id_application: 3
+    };
+    this.http
+      .post(`${this.local.getSettings()["API_URL"]}/auth/login`, user, {
+        headers: {},
+        observe: "response" as "response",
+        withCredentials: true
+      })
+      .pipe(
+        catchError(err => {
+          throw err;
+        })
+      )
+      .subscribe(
+        data => {
+          this.storage.set("user", data.body);
+          reponse = data;
+          if (data.body["access_token"]) {
+          } else {
+            console.warn(
+              "nous n'arrivons pas a trouver le token.\nModifiez l'api de manière à ce qu'elle renvoi le token"
+            );
+          }
+        },
+        err => {
+          reponse = err;
+        }
+      );
+  }
+
   // Save result of API requests
   public setLocalData(key, data) {
     //this.storage.set(`${API_STORAGE_KEY}-${key}`, data);
     this.storage.set(`${key}`, data);
   }
- 
+
   // Get cached API result
   public getLocalData(key) {
     //return this.storage.get(`${API_STORAGE_KEY}-${key}`);
     return this.storage.get(`${key}`);
-    
   }
 }
 
